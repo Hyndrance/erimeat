c6370771bc355ecbd209fa4b3ac79d7252b2e4b2
designkid <robertcdc21@gmail.com>
2018-03-18 17:37:29 +0800

    Modified client template and footer

--- admin/clientDetail.php
+++ admin/clientDetail.php
@@ -9,41 +9,43 @@ function getJobFunction($Id){
 ?>
 
 <div class="container container-fluid">
-  <div class="col-12">
-
+  <div class="col-12 m-t-30">
     <h2><?=$company->name;?></h2>
-    Business Number: <?=$company->abn;?><br>
-    Username: <?=$company->username;?>
+    <div class="row p-t-10 p-b-10">
+    <div class="col-12">
+      <div class="col-lg-4">
+        <label class="m-r-5">Department: </label><?=$company->department;?>
+        <br>
+        <i class="fa fa-phone m-r-5"></i><?=$company->phoneNumber;?>
+      </div>
+      <div class="col-lg-4">
+        <label class="m-r-5">Contact Person: </label><?=$company->contactPerson;?><br>
+        <i class="fa fa-mobile-phone m-r-5"></i><?=$company->mobileNumber;?>
+      </div>
+      <div class="col-lg-4">
+        <label class="m-r-5">Email: </label><?=$company->email;?><br>
+        <i class="fa fa-map-marker m-r-5"></i><?=$company->address;?>
 
-    <div class="row">
-      <div class="col-12">
-        <div class="col-lg-4">
-
-          name: <?=$company->address;?>
-<br>
-                  name: <?=$company->phoneNumber;?>
-        </div>
-        <div class="col-lg-4">
-              name: <?=$company->contactPerson;?><br>
-                            name: <?=$company->mobileNumber;?>
-        </div>
-        <div class="col-lg-4">
-
-                  name: <?=$company->email;?><br>
-
-                                name: <?=$company->department;?>
-        </div>
       </div>
     </div>
-
-              Job Category: <?=getJobFunction($company->jobFunctionId);?>
-
-
-
+    </div>
+    <h4 class="m-t-30 m-b-30">Job Category: <?=getJobFunction($company->jobFunctionId);?></h4>
+    <hr>
+    <?=$company->description;?>
               <!-- // foreach ($company as $key => $value) {
               //   echo $key . ": " . $value . "<br>";
               // } -->
 
   </div> <!-- end col -->
-
+  <div class="center-page text-center p-t-10 m-b-30">
+    <h4>Jobs</h4>
+    <div class="row">
+      <button class="btn btn-success stepy-finish" onclick="location.href='?view=jobList&abn=<?=$company->abn;?>&isApproved=1'">
+        Ongoing: <?=job()->count("abn=$company->abn and isApproved=1")?>
+      </button>
+      <button class="btn btn-warning stepy-finish" onclick="location.href='?view=jobList&abn=<?=$company->abn;?>&isApproved=0'">
+        Requests: <?=job()->count("abn=$company->abn and isApproved!=1")?>
+      </button>
+    </div>
+  </div>
 </div>
