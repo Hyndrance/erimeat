5a3902abc5aeff375a4c9ec4277196bf4ca2f9dd
acknolodgia <torredale1014@gmail.com>
2018-03-18 17:30:42 +0800

    Make candidate detail dynamic

--- home/candidateDetail.php
+++ home/candidateDetail.php
@@ -1,51 +1,39 @@
+<?php
+$Id = $_GET['Id'];
+$resume = resume()->get("Id='$Id'");
+
+function getJobFunction($Id){
+  $jf = job_function()->get("Id='$Id'");
+  echo $jf->option;
+}
+
+function getCity($Id){
+  $city = city_option()->get("Id='$Id'");
+  echo $city->city;
+}
+?>
 
 <div class="container-fluid m-t-30">
   <div class="row center-page container">
-    <h1> Job Position </h1>
-    <b>Reference: </b> 21312321321
+    <h1><?=getJobFunction($resume->jobFunctionId);?></h1>
+    <b>Employee Reference #: </b> <?=$resume->refNum;?>
 
     <div class="col-12 row">
       <div class="col-md-4">
-        <i class="fa fa-map-marker"></i> Address 1
+        <i class="fa fa-map-marker"></i> <?=$resume->address1;?>
       </div>
       <!-- College -->
       <div class="col-md-4">
-        <i class="fa fa-map-o"></i> Address 2
+        <i class="fa fa-map-o"></i> <?=$resume->address2;?>
       </div>
       <!-- Experience -->
       <div class="col-md-4 m-b-10">
-        <i class="fa fa-globe"></i> 6100
+        <i class="fa fa-globe"></i> <?=getCity($resume->city);?> <?=$resume->state;?> <?=$resume->zipCode;?>
       </div>
     </div>
-        <ul class="nav nav-tabs navtab-bg nav-justified">
-            <li class="active" style="background-color:#f2f2f2; border-radius: 5px; color: #fff;">
-                <a href="#home1" data-toggle="tab" aria-expanded="false">
-                    <span class="visible-xs"><i class="fa fa-home"></i></span>
-                    <h3 class="hidden-xs">Skills</h3>
-                </a>
-            </li>
-            <li class="" style="background-color: #f2f2f2; border-radius:5px;">
-                <a href="#profile1" data-toggle="tab" aria-expanded="true">
-                    <span class="visible-xs"><i class="fa fa-user"></i></span>
-                    <h3 class="hidden-xs">Description</h3>
-                </a>
-            </li>
-
-        </ul>
-        <div class="tab-content candidate-tab-border">
-        <div class="tab-pane active candidate-tab-content" id="home1">
-          <span>Graphics, Accounts Receivable, Accounts Payable, Billing, Microsoft, Supply Chain Management, Supplies, Answering Phones,
-            Ms Access, Filings, Reports, Reporting, Records</span>
-        </div>
-        <div class="tab-pane candidate-tab-content" id="profile1">
-          <span>
-            As a mid-level manager with ten years of experience, this job seeker most recently worked as a graphic specialist.
-            <br>
-            Background knowledge of accounting include his experience with accounts payable, accounts receivable, and billing.
-            <br>
-            She graduated with a bachelor's degree, in addition to her professional experience.
-          </span>
-        </div>
+      <hr>
+      <h3>Cover Letter</h3>
+      <p><?=$resume->coverLetter;?></p>
       </div>
     <div align="center" class="m-t-30 m-b-30">
       <div>
