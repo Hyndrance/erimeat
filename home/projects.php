edfdbf660947ad0c5068c0db200f020765cfacfb
designkid <robertcdc21@gmail.com>
2018-03-26 16:15:12 +0800

    Modified projects page

--- home/projects.php
+++ home/projects.php
@@ -15,8 +15,36 @@ function formatDate($val){
 
 <div style="position: relative;">
   <img style="position: absolute; top:0; right:0; height: 300px;" src="../include/assets/images/homepage-bg-1.png">
-<div class="container-80 container-fluid m-b-30">
+<div class="container container-fluid m-b-30">
 <h2 class="m-t-20 text-center">Supply Chain Projects</h2>
+<hr>
+
+
+<!-- Start of Static Projects List-->
+
+<div class="col-12">
+  <div class="col-lg-3" style="height: 300px;">
+    <ul style="list-style-type: none; padding-left: 10px !important; padding: 10px;">
+      <li style="border-bottom: 1.5px solid #e9e9e9; padding: 8px;" class="text-white bg-primary">Sample Lorem Ipsum</li>
+      <li style="border-bottom: 1.5px solid #e9e9e9; padding: 8px;">Lorem Ipsum</li>
+      <li style="border-bottom: 1.5px solid #e9e9e9; padding: 8px;">Lorem Ipsum</li>
+      <li style="border-bottom: 1.5px solid #e9e9e9; padding: 8px;">Lorem Ipsum</li>
+      <li style="border-bottom: 1.5px solid #e9e9e9; padding: 8px;">Lorem Ipsum</li>
+    </ul>
+  </div>
+  <div class="col-lg-9">
+    <img class="pull-right" src="../include/assets/images/aboutus-img.png">
+    <h3 class="text-blue">Sample Lorem Ipsum</h3>
+
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  </div>
+</div>
+
+<!-- End of Static List -->
+
+
   <!-- Start About Us Content -->
   <div class="center-page container-80">
     <?php if(!$projectList){?>
