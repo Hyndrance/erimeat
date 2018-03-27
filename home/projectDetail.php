c1176b719e2e6bf0b7ccc1cb613d8dd859feea7c
acknolodgia <torredale1014@gmail.com>
2018-03-27 16:04:06 +0800

    Fix error in projects

--- home/projectDetail.php
+++ home/projectDetail.php
@@ -1,10 +1,17 @@
 <?php
-$Id = $_GET['Id'];
+$Id = (isset($_GET['Id']) && $_GET['Id'] != '') ? $_GET['Id'] : '';
 $projects = projects()->get("Id='$Id'");
 
 ?>
-<img class="pull-right" src="../media/<?=$projects->uploadedImage;?>">
-<h3 class="text-blue"><?=$projects->title;?></h3>
+<?php if(!$projects){?>
+  <p>Teamire Project Professionals involvement and thousands of hours of
+    experience hold them in high esteem for managing various types of Supply Chain Projects
+    which were not only challenging but that required high degree of accuracy in analysis.
+  </p>
+<?php }else{?>
+  <img class="pull-right" src="../media/<?=$projects->uploadedImage;?>">
+  <h3 class="text-blue"><?=$projects->title;?></h3>
 
-<p><?=$projects->content;?></p>
-<br>
+  <p><?=$projects->content;?></p>
+  <br>
+<?php }?>
