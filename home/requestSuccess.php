2fc45cc7171ed2eb1613b7a82aff1f0939e84855
acknolodgia <torredale1014@gmail.com>
2018-04-22 22:31:49 +0800

    Update request talent success messages.

--- home/requestSuccess.php
+++ home/requestSuccess.php
@@ -7,23 +7,21 @@ $job = job()->count("workEmail='$email'");
   if($job>1){
 ?>
   <div class="container-80 container-fluid text-center m-t-30 m-b-30">
-    <h3>Thank You for your interest.</h3><br>
     <p>
-      It appears like you're already registered, please use your existing login credentials.<br>
-      Alternatively, you can contact our hr team for assistance<br>
-      hrcoordinator@teamire.com or call +61 452 364 793.
+      It appears you're already registered, please use your existing login credentials.<br>
+      Alternatively, you can contact our hr team for assistance.<br>
+      Click <a href="http://teamire.com/company/?view=login">here</a> to login.<br><br>
+      <i class="fa fa-phone"></i> +61 452 364 793<br>
+      <i class="fa fa-envelope"></i> hrcoordinator@teamire.com
     </p>
   </div>
 <?php
 }else{
 ?>
   <div class="container-80 container-fluid text-center m-t-30 m-b-30">
-    <h3>Thank You for your interest.</h3><br>
     <p>
-      We are on the midst of processing your talent request in order for you to proceed we're<br>
-      inviting you to register your company to our website for you to have access on you Hiring Dashboard.<br>
-      Alternatively, you can contact our hr team for assistance<br>
-      hrcoordinator@teamire.com or call +61 452 364 793.
+      Please note in order to request talent, we would need you to complete our new<br>
+      "Client Registration" click <a href="http://teamire.com/home/?view=clientForm">here</a> to proceed.
     </p>
   </div>
 <?php
